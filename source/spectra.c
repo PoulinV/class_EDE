8d5cd06566d514a8a07a4d26124528151b758bce
Vivian Poulin <poulin@lapth.cnrs.fr>
2019-01-18 10:10:44 -0500

    remove useless printfs

--- source/spectra.c
+++ source/spectra.c
@@ -3219,13 +3219,14 @@ int spectra_pk(
              psp->error_message,
              psp->error_message);
 
-  if (psp->spectra_verbose>0)
+  if (psp->spectra_verbose>0){
     fprintf(stdout," -> sigma8=%g (computed till k = %g h/Mpc)\n",
             psp->sigma8,
             exp(psp->ln_k[psp->ln_k_size-1])/pba->h);
     fprintf(stdout," -> S8=%g (computed till k = %g h/Mpc)\n",
             psp->sigma8*pow((1-pba->Omega0_lambda)/0.3,0.5),
             exp(psp->ln_k[psp->ln_k_size-1])/pba->h);
+  }
 
   if(pba->has_ncdm){
     class_call(spectra_sigma_cb(pba,ppm,psp,8./pba->h,0.,&(psp->sigma8_cb)),
